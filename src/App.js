bfd3b827e920a92de9d8dd20fb0dbd043236e373
janmmeier <Jan-Markus.Meier@tptlive.ee>
2021-09-23 21:08:17 +0300

    fetch movie data from API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
+// Routing
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //components
 import Header from './Components/Header';
 import Home from './Components/Home';
+import Movie from './Components/Movie';
+import NotFound from './Components/NotFound';
 
 //styles
 import { GlobalStyle } from './GlobalStyle';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-      <GlobalStyle/>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/:movieId' element={<Movie/>} />
+      <Route path='/*' element={<NotFound/>} />
+    </Routes>
+    <GlobalStyle/>
+  </BrowserRouter>
+
+);
 
 export default App;
