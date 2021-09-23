485b49aaa56ac9deb029afd7a506729c9afdc7be
janmmeier <Jan-Markus.Meier@tptlive.ee>
2021-09-23 18:10:05 +0300

    added spinner + search bar w function

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 //API
 import API from '../API';
 //config
@@ -7,26 +7,29 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
+import Spinner from "./Spinner";
+import SearchBar from "./SearchBar";
 //hook
 import { useHomeFetch} from '../Hooks/useHomeFetch'
 //image
 import NoImage from '../images/no_image.jpg'
 
 const Home = () => {
-    const {state, loading, error} = useHomeFetch();
+    const {state, loading, error, setSearchTerm, searchTerm} = useHomeFetch();
 
     console.log(state);
 
     return (
         <>
-            {state.results[0] ? (
+            {!searchTerm && state.results[0] ? (
                 <HeroImage
                     image = {`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
                     title = {state.results[0].original_title}
                     text = {state.results[0].overview}
                     />
             ) : null}
-            <Grid header = 'Popular Movies'>
+            <SearchBar setSearchTerm={setSearchTerm} />
+            <Grid header = {searchTerm ? 'Search Result':'Popular Movies'}>
                 {state.results.map(movie => (
                     <Thumb
                         key={movie.id}
@@ -39,8 +42,8 @@ const Home = () => {
                         movieId={movie.id}
                     />
                 ))}
-
             </Grid>
+            <Spinner />
         </>
     )
 };
--- src/Hooks/useHomeFetch.js
+++ src/Hooks/useHomeFetch.js
@@ -11,6 +11,7 @@ const initialState = {
 
 export const useHomeFetch = () => {
     
+    const [searchTerm, setSearchTerm] = useState('');
     const [state, setState] = useState(initialState);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
@@ -33,10 +34,11 @@ export const useHomeFetch = () => {
         }
 
     };
-
+    //initial and search
     useEffect(() => {
-        fetchMovies(1)
-    }, []);
+        setState(initialState)
+        fetchMovies(1, searchTerm)
+    }, [searchTerm]);
 
-    return { state, loading, error};
+    return { state, loading, error, setSearchTerm, searchTerm };
 };
